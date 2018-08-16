4de9303b3ecf239165f6cf006faa2cc5b50cbe2b
Sheen <seanmac11741@gmail.com>
2018-08-15 21:39:11 -0600

    Initial commit

---
+++ app/src/main/java/com/sheen/sidequester/MainActScreenFragment.java
@@ -0,0 +1,22 @@
+package com.sheen.sidequester;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActScreenFragment extends Fragment {
+
+    public MainActScreenFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_act_screen, container, false);
+    }
+}
